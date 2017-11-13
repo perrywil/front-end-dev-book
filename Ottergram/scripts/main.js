b80bd4d43c1933342c819a266d77db6c28965e03
perrywil <william.perry.r@gmail.com>
2017-11-13 22:15:28 +0100

    function hideDetails()

--- Ottergram/scripts/main.js
+++ Ottergram/scripts/main.js
@@ -1,6 +1,7 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -39,6 +40,11 @@ function getThumbnailsArray() {
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
 function initializeEvents() {
